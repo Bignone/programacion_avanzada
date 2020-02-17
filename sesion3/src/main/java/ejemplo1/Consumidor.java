94e2f39dc9aeceeaaf8787831c7ea507a63ad6b9
ebustos <ebustos@indra.es>
2020-02-17 10:11:49 +0100

    minor changes

--- sesion3/src/main/java/ejemplo1/Consumidor.java
+++ sesion3/src/main/java/ejemplo1/Consumidor.java
@@ -40,4 +40,6 @@ public class Consumidor extends Thread
         }
        
     }
+
+
 }
--- sesion3/src/main/java/ejemplo1/PruebaBuzon.java
+++ sesion3/src/main/java/ejemplo1/PruebaBuzon.java
@@ -24,4 +24,6 @@ public class PruebaBuzon
         consumerAna.start();
         consumerMaria.start();
     }
+
+
 }
