2dc764c47be9e7a7e1a1ca66567dcc9ac98a9704
ebustosm6 <eduardobustosmiranda@hotmail.com>
2020-03-30 21:50:40 +0200

    Gamificacion Bloque 3 Tarea 2

---
+++ GamificacionBloque3Tarea2/src/main/java/bloque3tarea2/Main.java
@@ -0,0 +1,33 @@
+package bloque3tarea2;
+
+import bloque3tarea2.Consumer;
+import bloque3tarea2.Producer;
+
+public class Main {
+	
+	private static int NUM_PRODUCERS_CREATED = 3;
+	private static int NUM_PRODUCERS_MESSAGES = 15;
+	private static int NUM_READERS_CREATED = 2;
+
+	public static void main(String[] args) {
+		Thermometer thermometer = new Thermometer();
+  
+		// More producers -> more fun
+        Producer sensor;
+        for (int i = 0; i < NUM_PRODUCERS_CREATED; i++)
+        {
+        	sensor = new Producer("Sensor" + i, NUM_PRODUCERS_MESSAGES, thermometer);
+        	sensor.start();
+        }
+        
+        // More consumers -> even more fun!
+        Consumer display;
+        for (int i = 0; i < NUM_READERS_CREATED; i++)
+        {
+        	display= new Consumer("Reader" + i, thermometer);
+        	display.start();
+        }
+        
+        
+        }
+}
---
+++ GamificacionBloque3Tarea2/src/main/java/bloque3tarea2/Thermometer.java
@@ -0,0 +1,85 @@
+package bloque3tarea2;
+
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Thermometer {
+	
+	private static int MEMORY_SIZE = 15;
+	private Lock lock = new ReentrantLock();
+    private Condition spaceInMemory = lock.newCondition();
+    private Condition valuesInMemory = lock.newCondition();
+
+	ArrayList<Double> memory = new ArrayList<>(MEMORY_SIZE);
+    
+	public boolean isMemoryFull() {
+		return memory.size() == MEMORY_SIZE;
+	}
+	
+	public boolean isMemoryEmpty() {
+		return memory.isEmpty();
+	}
+	
+	public void addtoMemory(double value) {
+		memory.add(value);
+	}
+	
+	public double readFromMemory() {
+		return memory.remove(0);
+	}
+	
+	synchronized public double memoryAccess(String operation, double value) {
+    	double result = -99.9;
+    	if (operation.equals("get")) {
+    		result = memory.remove(0);
+    	}
+    	else if (operation.equals("put")) {
+    		memory.add(value);
+    	}
+    	return result;
+    }
+    
+    public void printMemory() {
+    	System.out.println("Memory: " + memory.toString());
+    }
+    
+    public void sendMessage(double num, String productorName) {
+    	try {
+            lock.lock();
+            while (isMemoryFull()) {
+                try {
+                	spaceInMemory.await();
+                } catch (Exception e) {
+                }
+            }
+            addtoMemory(num);
+            System.out.println("Producer " + productorName + " writes numer " + num);
+            printMemory();
+            valuesInMemory.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    public double receiveMessage() {
+    	try {
+            lock.lock();
+            while (isMemoryEmpty()) {
+                try {
+                	valuesInMemory.await(); // espera a que le manden una señal
+                } catch (Exception e) {
+                }
+            }
+            double readed = readFromMemory();
+            spaceInMemory.signalAll();
+            return readed;
+        } finally {
+        	lock.unlock();
+        }
+    }
+	
+
+}
