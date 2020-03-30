d8d6d24f3cb0062b3c47b4ddef609d31e232e255
ebustosm6 <eduardobustosmiranda@hotmail.com>
2020-03-30 21:31:45 +0200

    final

---
+++ GamificacionBloque3Tarea3/src/main/java/bloque3tarea3/Producer.java
@@ -0,0 +1,41 @@
+package bloque3tarea3;
+
+public class Producer extends Thread {
+
+    private String name;
+    private int numMessages;
+    private Thermometer thermometer;
+
+    public Producer(String name, int numMessages, Thermometer thermometer) {
+        this.name = name;
+        this.numMessages = numMessages;
+        this.thermometer = thermometer;
+    }
+    
+    private double generateRandomNumber() {
+    	return Math.random() * 30;
+    }
+    
+    public void goToSleep() throws InterruptedException {
+    	sleep((int) (300 + 400 * Math.random()));
+    }
+    
+    public void putIntoThermometer() {
+    	thermometer.sendMessage(generateRandomNumber(), name);
+    }
+     
+    public void run() {
+    	for (int i = 1; i <= numMessages; i++) {
+    		try {
+    			goToSleep();
+    			putIntoThermometer();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+    		
+    		
+    	}
+    }
+
+
+}
---
+++ GamificacionBloque3Tarea3/src/main/java/bloque3tarea3/Thermometer.java
@@ -0,0 +1,67 @@
+package bloque3tarea3;
+
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+
+public class Thermometer {
+	
+	private static int NUM_WRITERS = 5;
+	private static int NUM_READERS = 5;
+	private Semaphore semaphoreWriters = new Semaphore(NUM_WRITERS);
+	private Semaphore semaphoreReaders = new Semaphore(NUM_READERS);
+	ArrayList<Double> memory = new ArrayList<>(10);
+    
+	
+	public Thermometer() {
+    	try {
+			semaphoreReaders.acquire(NUM_READERS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    }
+	
+	synchronized public double memoryAccess(String operation, double value) {
+    	double result = -99.9;
+    	if (operation.equals("get")) {
+    		result = memory.remove(0);
+    	}
+    	else if (operation.equals("put")) {
+    		memory.add(value);
+    	}
+    	return result;
+    }
+    
+    public void printMemory() {
+    	System.out.println("Memory: " + memory.toString());
+    }
+    
+    public void sendMessage(double num, String productorName) {
+    	try {
+    		semaphoreWriters.acquire();//Si el contador es cero el thread se duerme, de lo contrario se reduce y obtiene el acceso  
+    		memoryAccess("put", num);
+	        System.out.println("Productor " + productorName +" add number: " + num); 
+	        printMemory();
+	        
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	} finally {
+    		semaphoreReaders.release();//Libera el semaphore e incrementa el countador 
+    	}
+    }
+    
+    public double receiveMessage() {
+    	double readed = -999.99;
+    	try {
+    		semaphoreReaders.acquire();//Si el contador es cero el thread se duerme, de lo contrario se reduce y obtiene el acceso  
+    		readed = memoryAccess("get", 0);
+	        
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	} finally {
+    		semaphoreWriters.release();//Libera el semaphore e incrementa el countador 
+    	}
+    	return readed;
+    }
+	
+
+}
